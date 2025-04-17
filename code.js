ae6ba3d1e6bd6b7c20c54a26d170582f34c46501
Zacherobay <zzboy1767@gmail.com>
2025-04-17 15:05:45 -0700

    uploaded assignment 12.1 files

---
+++ code.js
@@ -0,0 +1,8 @@
+// this code is for the index and boards pages' buttons //
+function homeBtn() {
+window.location.href = "index.html";
+}
+
+function boardsBtn() {
+window.location.href = "boards.html";
+}
